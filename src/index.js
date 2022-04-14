3d2241d6e66ac27ffd49caa51649cbec3861feb3
Jovan Jocic <jovan.jocic@quantox.com>
2022-04-14 23:26:50 +0200

    Initial setup - boilerplate

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
